51a5c59d0a5430f20120e560c73dfb7c885d6e5a
Nikita <mazus201@gmail.com>
2020-09-02 12:29:27 +0300

    experiments with OOP

--- JavaScript/OOP.js
+++ JavaScript/OOP.js
@@ -7,11 +7,13 @@ const mazda5 = {
     type: "pessenger"
 };
 
+//let mazda5_1 = Object.create(mazda5); способ создания дочерней для mazda5 переменной в процессе ее объявления 
+
 let mazda5_1 = {
     powerEngine: 250,
     color: "red"
 };
 
-Object.setPrototypeOf(mazda5_1, mazda5);
+Object.setPrototypeOf(mazda5_1, mazda5); //объявление дочерней для mazda5 переменной и добавление в нее новых значений
 
 console.log(mazda5_1);
